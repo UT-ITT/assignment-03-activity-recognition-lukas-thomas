e52573c93e25bae5b9fdafc91ef924eae73d5ebc
Luk38 <85354311+Luk38@users.noreply.github.com>
2026-05-16 23:06:59 +0200

    basic sensor template

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -1,9 +1,18 @@
 # this program visualizes activities with pyglet
 
-#import activity_recognizer as activity
+import time
+import activity_recognizer as activity
+from preprocessing import extract_features
 import pyglet
 from DIPPID import SensorUDP
-import os 
+import pandas as pd
+import os
+
+# Todos
+# Read in data from dippid device
+# Transform data with functions for the model
+# Run model to get activity prediction
+# Scale all images to same size
 
 # Windows settings
 WINDOW_WIDTH = 800
@@ -24,6 +33,11 @@ TEXT_FONT_NAME = 'Arial'
 
 # create sensor
 sensor = SensorUDP(PORT)
+data = [] # current data buffer
+DATA_PATH = "data"
+
+# game parameters
+activity_name = "lifting"  # current activity
 
 # create window
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -56,7 +70,6 @@ lifting_sprite = pyglet.sprite.Sprite(lifting_animation, ANIMATION_POSITION[0], 
 rowing_sprite = pyglet.sprite.Sprite(rowing_animation, ANIMATION_POSITION[0], ANIMATION_POSITION[1])
 running_sprite = pyglet.sprite.Sprite(running_animation, ANIMATION_POSITION[0], ANIMATION_POSITION[1])
 
-print(jumping_jacks_sprite.width, jumping_jacks_sprite.height)
 # Scale sprites to fit the window
 jumping_jacks_sprite.update(scale=0.3)
 lifting_sprite.update(scale=0.3)
@@ -68,7 +81,49 @@ lifting_sprite.update(x=ANIMATION_POSITION[0] - lifting_sprite.width // 2)
 rowing_sprite.update(x=ANIMATION_POSITION[0] - rowing_sprite.width // 2)
 running_sprite.update(x=ANIMATION_POSITION[0] - running_sprite.width // 2)
 
-print(jumping_jacks_sprite.width, jumping_jacks_sprite.height)
+def train_classifier():
+    X, y, persons, encoder = activity.load_and_preprocess_data(DATA_PATH)
+    clf = activity.get_pipeline()
+    activity.run_single_evaluation(X, y, persons, 42, encoder, clf)
+
+    return clf
+
+def read_sensor_data(dt):
+    acc = sensor.get_value("accelerometer")
+    gyro = sensor.get_value("gyroscope")
+    timestamp = time.time()
+
+    if len(data) > 1002:  # limit buffer size to last 1000 readings
+        data.pop(0)
+    # only save if both sensors exist
+    if acc is not None and gyro is not None:
+        row = {
+            "timestamp": timestamp,
+            "acc_x": acc["x"],
+            "acc_y": acc["y"],
+            "acc_z": acc["z"],
+            "gyro_x": gyro["x"],
+            "gyro_y": gyro["y"],
+            "gyro_z": gyro["z"]
+        }
+
+        data.append(row)
+    df = pd.DataFrame(data)
+    return df
+ 
+# extract features 
+def transform_data_for_model(df):
+    return extract_features(df)
+
+def predict_activity(classifier, transformed_data):
+    return classifier.predict(transformed_data)
+
+def update(dt, classifier):
+    global activity_name
+    df = read_sensor_data(dt)
+    transformed_data = transform_data_for_model(df)
+    activity_name = predict_activity(classifier, transformed_data)
+    text.text = f'Current Activity: {activity_name}'
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -79,7 +134,6 @@ def on_key_press(symbol, modifiers):
 def on_draw():
     pyglet.gl.glClearColor(BACKGROUND_COLOR[0]/255, BACKGROUND_COLOR[1]/255, BACKGROUND_COLOR[2]/255, 1)
     window.clear()
-    activity_name = "lifting"  # Replace with actual activity recognition result
     
     if activity_name == "jumping_jacks":
         jumping_jacks_sprite.draw()
@@ -89,8 +143,11 @@ def on_draw():
         rowing_sprite.draw()
     elif activity_name == "running":
         running_sprite.draw()
-    
-    text.text = f'Current Activity: {activity_name}'
+        
     text.draw()
 
+    print(data)
+    print(read_sensor_data(0))  # Print sensor data to console for debugging
+    print(clf)
+clf = train_classifier()
 pyglet.app.run()
