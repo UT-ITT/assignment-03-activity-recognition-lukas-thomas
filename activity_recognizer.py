69eef56c237fe78d3350d89c540f21a11950246a
Tholuc <50240374+Tholuc@users.noreply.github.com>
2026-05-16 12:36:40 +0200

    feature

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -14,7 +14,7 @@ from preprocessing import create_feature_dataset
 
 # Constants
 SEED = 1000
-DATA_PATH = "data"
+DATA_PATH = "data_without_sam"
 SEEDS_FOR_TESTING = [5, 10, 42, 100, 420, 1000, 21, 99, 88, 69]
 
 def load_and_preprocess_data(path):
